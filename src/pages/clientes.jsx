135914b3ec4321207eacc145ec30f39eaf9d7b87
Sidney Rafael Mantovani Ribeiro <sidneyrafael191@gmail.com>
2022-12-01 04:45:05 -0300

    ajustes de layout

--- src/pages/clientes.jsx
+++ src/pages/clientes.jsx
@@ -5,7 +5,7 @@ import clients from '../data/lista_clientes.json'
 
 import users from '../data/lista_usuario.json'
 
-{console.log(users.users.map((user) => (user.login)))}
+{ console.log(users.users.map((user) => (user.login))) }
 export default function clientes() {
 
     // function isEqual(elem, index, array) {
@@ -28,31 +28,31 @@ export default function clientes() {
             <Layout titulo='Escolha o cliente'>
                 <div className={styles.cla}>
 
-                <table className='table'>
-                    <thead>
-                        <tr>
-                            <th width='15%'>Id</th>
-                            <th width='40%'>Nome</th>
-                            <th width='25%'>Cidade</th>
-                            <th width='15%'></th>
-                        </tr>
-                    </thead>
-                    <tbody>
-                        {console.log(clients.clientes.map((cli) => (cli.id)))}
-                        {clients.clientes.map((client, index) => (
-                    <tr key={index}>
-                        <td>{client.id}</td>
-                        <td>{client.nome}</td>
-                        <td>{client.cidade}</td>
-                        <td width='15%'>
-                            <a placeholder='Vender' href={'/'}>Vender
-                            </a>
-                        </td>
-                    </tr>
+                    <table className='table'>
+                        <thead>
+                            <tr>
+                                <th width='15%'>Id</th>
+                                <th width='40%'>Nome</th>
+                                <th width='25%'>Cidade</th>
+                                <th width='15%'></th>
+                            </tr>
+                        </thead>
+                        <tbody>
+                            {console.log(clients.clientes.map((cli) => (cli.id)))}
+                            {clients.clientes.map((client, index) => (
+                                <tr key={index}>
+                                    <td>{client.id}</td>
+                                    <td>{client.nome}</td>
+                                    <td>{client.cidade}</td>
+                                    <td width='15%'>
+                                        <a placeholder='Vender' href={'/'}>Vender
+                                        </a>
+                                    </td>
+                                </tr>
 
-                ))}
-                    </tbody>
-                </table>
+                            ))}
+                        </tbody>
+                    </table>
                 </div>
             </Layout>
         </div>
